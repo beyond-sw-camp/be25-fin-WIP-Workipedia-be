13433da756adff5ec238e3b13baca2f5f182ac9b
gahyoung-eng <gahyoung920@gmail.com>
2026-06-04 10:32:06 +0900

    refactor: 관리자 API 응답 포맷 및 Lombok 적용

--- src/main/java/com/wip/workipedia/admin/domain/AdminLog.java
+++ src/main/java/com/wip/workipedia/admin/domain/AdminLog.java
@@ -9,7 +9,9 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import java.time.LocalDateTime;
+import lombok.Getter;
 
+@Getter
 @Entity
 @Table(name = "admin_logs")
 public class AdminLog {
