ab1f0f9843ac441e978ec655d866a0381c3f584e
gahyoung-eng <gahyoung920@gmail.com>
2026-06-04 12:46:49 +0900

    refactor: 뱃지 도메인 제거

--- src/main/java/com/wip/workipedia/badge/repository/BadgeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.wip.workipedia.badge.repository;
-
-import com.wip.workipedia.badge.domain.Badge;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BadgeRepository extends JpaRepository<Badge, Long> {
-}
--- src/main/java/com/wip/workipedia/badge/repository/UserBadgeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.wip.workipedia.badge.repository;
-
-import com.wip.workipedia.badge.domain.UserBadge;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserBadgeRepository extends JpaRepository<UserBadge, Long> {
-}
