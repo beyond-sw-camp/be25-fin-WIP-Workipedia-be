d96027205c180edc0087b2e14164e2cfc982fe34
gahyoung-eng <gahyoung920@gmail.com>
2026-06-05 11:55:39 +0900

    refactor: 관리자 skeleton 제거 및 명세 정리

--- src/main/java/com/wip/workipedia/admin/repository/AdminLogRepository.java
+++
@@ -1,7 +0,0 @@
-package com.wip.workipedia.admin.repository;
-
-import com.wip.workipedia.admin.domain.AdminLog;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AdminLogRepository extends JpaRepository<AdminLog, Long> {
-}
