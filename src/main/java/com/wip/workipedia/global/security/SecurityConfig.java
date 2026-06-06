2e4b526b02e76f3354cc3bc8dda1ed728b06b0f1
JK.Min <75975464+calendar3450@users.noreply.github.com>
2026-06-07 00:57:22 +0900

    Refactor: 로그인 보안 부분 API 해제

--- src/main/java/com/wip/workipedia/global/security/SecurityConfig.java
+++ src/main/java/com/wip/workipedia/global/security/SecurityConfig.java
@@ -42,6 +42,8 @@ public class SecurityConfig {
 				.requestMatchers(HttpMethod.POST, "/api/v1/auth/password-reset/code").permitAll()
 				.requestMatchers(HttpMethod.POST, "/api/v1/auth/password-reset/code/verify").permitAll()
 				.requestMatchers(HttpMethod.PATCH, "/api/v1/auth/password-reset").permitAll()
+				.requestMatchers("/api/v1/worki/**", "/api/v1/search/**").permitAll()
+				.requestMatchers("/api/v1/faq/**").permitAll()
 				.anyRequest().authenticated()
 			)
 			.build();
