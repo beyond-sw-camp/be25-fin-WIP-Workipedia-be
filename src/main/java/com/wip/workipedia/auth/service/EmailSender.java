77ccf2556fd40bc901e8b2a59893dde3a39b0961
0lthree <oithreed@gmail.com>
2026-06-07 14:22:15 +0900

    docs: 비밀번호 재설정 인증코드 관련 주석 정리

--- src/main/java/com/wip/workipedia/auth/service/EmailSender.java
+++ src/main/java/com/wip/workipedia/auth/service/EmailSender.java
@@ -1,6 +1,6 @@
 package com.wip.workipedia.auth.service;
 
-// 인증코드 이메일 발송 방식을 분리하기 위한 인터페이스입니다.
+// 이메일 인증코드 발송 규칙을 정의하고 방식을 분리하기 위한 인터페이스입니다.
 public interface EmailSender {
 
 	void sendSignupCode(String email, String code);
