f464060506607f0ba19359855a1545f1f4e3dd58
0lthree <oithreed@gmail.com>
2026-06-08 14:06:06 +0900

    feat: 알림 설정 변경 API 연결 및 기능 구현

--- src/main/java/com/wip/workipedia/mypage/controller/MyPageController.java
+++ src/main/java/com/wip/workipedia/mypage/controller/MyPageController.java
@@ -2,10 +2,15 @@ package com.wip.workipedia.mypage.controller;
 
 import com.wip.workipedia.mypage.dto.MyPageResponse;
 import com.wip.workipedia.mypage.service.MyPageService;
+import com.wip.workipedia.notification.dto.NotificationSettingRequest;
+import com.wip.workipedia.notification.service.NotificationSettingService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,6 +20,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class MyPageController {
 
 	private final MyPageService myPageService;
+	private final NotificationSettingService notificationSettingService;
 
 	// 마이페이지 조회
 	@GetMapping("/profile")
@@ -24,5 +30,19 @@ public class MyPageController {
 		MyPageResponse myPageResponse = myPageService.getMyPage(userId);
 
 		return ResponseEntity.ok(myPageResponse);
+	}
+
+	// 알림 설정 변경
+	@PatchMapping("/notification-settings")
+	public ResponseEntity<MyPageResponse.NotificationSettings> updateNotificationSettings(
+		@AuthenticationPrincipal Long userId,
+		@Valid @RequestBody NotificationSettingRequest notificationSettingRequest
+	) {
+		MyPageResponse.NotificationSettings notificationSettings = notificationSettingService.update(
+			userId,
+			notificationSettingRequest
+		);
+
+		return ResponseEntity.ok(notificationSettings);
 	}
 }
