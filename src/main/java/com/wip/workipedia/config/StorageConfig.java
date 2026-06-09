b29c4ad736a6157b9be6b163322c9e5f720cc60d
jinddd3 <jinddd3@gmail.com>
2026-06-09 15:57:11 +0900

    feat: StorageConfig 조건부 Bean 등록, R2Config 제거

---
+++ src/main/java/com/wip/workipedia/config/StorageConfig.java
@@ -0,0 +1,26 @@
+package com.wip.workipedia.config;
+
+import com.wip.workipedia.storage.adapter.MinioStorageAdapter;
+import com.wip.workipedia.storage.adapter.R2StorageAdapter;
+import com.wip.workipedia.storage.port.StoragePort;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableConfigurationProperties(StorageProperties.class)
+public class StorageConfig {
+
+    @Bean
+    @ConditionalOnProperty(name = "storage.provider", havingValue = "r2")
+    public StoragePort r2StorageAdapter(StorageProperties props) {
+        return new R2StorageAdapter(props);
+    }
+
+    @Bean
+    @ConditionalOnProperty(name = "storage.provider", havingValue = "minio")
+    public StoragePort minioStorageAdapter(StorageProperties props) {
+        return new MinioStorageAdapter(props);
+    }
+}
