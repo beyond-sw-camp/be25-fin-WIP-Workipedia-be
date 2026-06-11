370d1b3e0d522aa4ffea5f025d1ce76ced467a5c
gahyoung-eng <gahyoung920@gmail.com>
2026-06-11 12:12:53 +0900

    관리자 워키 삭제 포인트 차감 정책 보완

--- src/main/java/com/wip/workipedia/admin/worki/controller/AdminWorkiQuestionController.java
+++ src/main/java/com/wip/workipedia/admin/worki/controller/AdminWorkiQuestionController.java
@@ -1,5 +1,6 @@
 package com.wip.workipedia.admin.worki.controller;
 
+import com.wip.workipedia.admin.worki.dto.AdminWorkiQuestionDeleteResponse;
 import com.wip.workipedia.admin.worki.service.AdminWorkiQuestionService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -17,8 +18,7 @@ public class AdminWorkiQuestionController {
 
 	// 부적절한 워키 게시글을 관리자 권한으로 삭제하고, 작성자 포인트를 차감한다.
 	@DeleteMapping("/{questionId}")
-	public ResponseEntity<Void> delete(@PathVariable Long questionId) {
-		adminWorkiQuestionService.delete(questionId);
-		return ResponseEntity.noContent().build();
+	public ResponseEntity<AdminWorkiQuestionDeleteResponse> delete(@PathVariable Long questionId) {
+		return ResponseEntity.ok(adminWorkiQuestionService.delete(questionId));
 	}
 }
