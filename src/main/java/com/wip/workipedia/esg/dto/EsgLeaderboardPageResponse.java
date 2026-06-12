60db2817c1430cb94ca7d8d3a46a19a717bfe39c
0lthree <oithreed@gmail.com>
2026-06-12 09:15:57 +0900

    chore: ESG 폴더 제거

--- src/main/java/com/wip/workipedia/esg/dto/EsgLeaderboardPageResponse.java
+++
@@ -1,9 +0,0 @@
-package com.wip.workipedia.esg.dto;
-
-import java.util.List;
-
-public record EsgLeaderboardPageResponse(
-	List<EsgLeaderboardResponse> topRankers,
-	EsgLeaderboardResponse myRank
-) {
-}
