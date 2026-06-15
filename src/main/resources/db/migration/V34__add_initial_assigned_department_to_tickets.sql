d0cdc46c6306d7c1feba5103c39643730708ef02
gahyoung-eng <gahyoung920@gmail.com>
2026-06-15 14:43:00 +0900

    fix: 마이그레이션 버전 수정

--- src/main/resources/db/migration/V34__add_initial_assigned_department_to_tickets.sql
+++ src/main/resources/db/migration/V34__add_initial_assigned_department_to_tickets.sql
@@ -1,18 +1,18 @@
 ALTER TABLE tickets
-    ADD COLUMN initial_assigned_department_id BIGINT NULL AFTER assigned_department_id;
+    ADD COLUMN initial_auto_assigned_department_id BIGINT NULL AFTER assigned_department_id;
 
 UPDATE tickets
-SET initial_assigned_department_id = assigned_department_id
-WHERE initial_assigned_department_id IS NULL
+SET initial_auto_assigned_department_id = assigned_department_id
+WHERE initial_auto_assigned_department_id IS NULL
   AND assigned_department_id IS NOT NULL
   AND routing_decision = 'AUTO_ASSIGNED'
   AND deleted_at IS NULL
   AND is_deleted = 'N';
 
 ALTER TABLE tickets
-    ADD CONSTRAINT fk_tickets_initial_assigned_department
-        FOREIGN KEY (initial_assigned_department_id)
+    ADD CONSTRAINT fk_tickets_initial_auto_assigned_department
+        FOREIGN KEY (initial_auto_assigned_department_id)
         REFERENCES departments(department_id);
 
-CREATE INDEX idx_tickets_initial_assigned_department_id
-    ON tickets (initial_assigned_department_id);
+CREATE INDEX idx_tickets_initial_auto_assigned_department_id
+    ON tickets (initial_auto_assigned_department_id);
