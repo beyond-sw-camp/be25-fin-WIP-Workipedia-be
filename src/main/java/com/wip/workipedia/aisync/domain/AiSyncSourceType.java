cc4169668c4f923ff0dbc6e3cf8e35c3a7de9419
jinddd3 <jinddd3@gmail.com>
2026-06-24 15:05:41 +0900

    feat: MANUAL_CHANGE_SUMMARY 잡 타입 추가 및 text 워커 claim 대상에 포함

--- src/main/java/com/wip/workipedia/aisync/domain/AiSyncSourceType.java
+++ src/main/java/com/wip/workipedia/aisync/domain/AiSyncSourceType.java
@@ -1,5 +1,5 @@
 package com.wip.workipedia.aisync.domain;
 
 public enum AiSyncSourceType {
-    MANUAL, WORKI, KNOWLEDGE_DATA, MANUAL_KNOWLEDGE, DEPT_RR
+    MANUAL, WORKI, KNOWLEDGE_DATA, MANUAL_KNOWLEDGE, DEPT_RR, MANUAL_CHANGE_SUMMARY
 }
